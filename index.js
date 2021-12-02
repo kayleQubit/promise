95506a9828968e65ada759655f8b40b48023bb8f
kayleQubit <38135651+kayleQubit@users.noreply.github.com>
2021-12-02 15:35:35 +0000

    Last time...

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const Promise = (resolver) => {
-  let promise
+const Promise = (promiseConstruct) => {
+  let promise = {
+    'then': then
+  }
+  const wait = []
 
+  promiseConstruct(resolve)
   return promise
+  
+  function then (newPromiseConstruct) {
+    return Promise(newPromiseConstruct)
+  }
+
+  function resolve (val) {
+    then.call(cb, val)
+  }
+
 }
 
 export { Promise }
+  
+
+
+
+
+
+
+const myPromise = new Promise((resolve) => {
+  window.setTimeout(() => { console.log('hello world!'); resolve(5)}, 1000)
+})
+
+myPromise
+  .then((resolve) => { resolve(32)})
+  .then((resolve) => { resolve(12) })
+
+  
